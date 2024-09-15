c791a1fd550a2c30be9cbfce5c2b63775975632a
Andrea01101 <162194766+Andrea01101@users.noreply.github.com>
2024-09-15 11:44:15 +0200

    Update DAO.py

--- database/DAO.py
+++ database/DAO.py
@@ -52,3 +52,82 @@ class DAO():
             cursor.close()
             cnx.close()
         return result
+
+    @staticmethod
+    def getAllYears():
+        cnx = DBConnect.get_connection()
+        result = []
+        if cnx is None:
+            print("Connessione fallita")
+        else:
+            cursor = cnx.cursor(dictionary=True)
+            query = """select distinct year(s.`datetime`) as year
+                       from sighting s 
+                       order by year desc"""
+            cursor.execute(query)
+
+            for row in cursor:
+                result.append(row["year"])
+            cursor.close()
+            cnx.close()
+        return result
+
+    @staticmethod
+    def getAllShapes(y: int):
+        cnx = DBConnect.get_connection()
+        result = []
+        if cnx is None:
+            print("Connessione fallita")
+        else:
+            cursor = cnx.cursor(dictionary=True)
+            query = """select distinct s.shape
+                       from sighting s 
+                       where year(s.`datetime`) = %s and s.shape != ""
+                       order by s.shape"""
+            cursor.execute(query, (y,))
+
+            for row in cursor:
+                result.append(row["shape"])
+            cursor.close()
+            cnx.close()
+        return result
+
+    @staticmethod
+    def get_all_nodes(y: int, s):
+        cnx = DBConnect.get_connection()
+        result = []
+        if cnx is None:
+            print("Connessione fallita")
+        else:
+            cursor = cnx.cursor(dictionary=True)
+            query = """select *
+                       from sighting s 
+                       where year(s.`datetime`) = %s and s.shape = %s"""
+            cursor.execute(query, (y, s))
+
+            for row in cursor:
+                result.append(Sighting(**row))
+            cursor.close()
+            cnx.close()
+        return result
+
+    @staticmethod
+    def get_all_edges(y: int, s, idMap):
+        cnx = DBConnect.get_connection()
+        result = []
+        if cnx is None:
+            print("Connessione fallita")
+        else:
+            cursor = cnx.cursor(dictionary=True)
+            query = """select s1.id as s1id, s2.id as s2id
+                       from sighting s1, sighting s2 
+                       where year(s1.`datetime`) = year(s2.`datetime`) and year(s1.`datetime`) = %s 
+                       and s1.shape = s2.shape and s1.shape = %s and s1.state = s2.state 
+                       and s1.`datetime` < s2.`datetime` """
+            cursor.execute(query, (y, s))
+
+            for row in cursor:
+                result.append((idMap[row['s1id']], idMap[row['s2id']]))
+            cursor.close()
+            cnx.close()
+        return result
